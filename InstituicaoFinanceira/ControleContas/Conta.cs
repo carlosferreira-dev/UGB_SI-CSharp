d6ee52b36de71d73fbad1d4f0d66b7648e8a3752
Carlos Eduardo Ferreira <mail@carlosferreira.dev>
2023-09-07 00:49:18 -0300

    Instituição Financeira

---
+++ InstituicaoFinanceira/ControleContas/Conta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleContas
+{
+    public class Conta
+    {
+        public Conta(long numero) 
+        {
+            Numero = numero;
+        }
+        public Conta() 
+        { 
+        }
+        public long Numero { get; private set; }
+        public decimal Saldo { get; set; }
+    }
+}
---
+++ InstituicaoFinanceira/ControleContas/Program.cs
@@ -0,0 +1,7 @@
+﻿using ControleContas;
+Conta conta1 = new Conta(123456);
+Conta conta2 = new Conta(654321);
+conta2.Saldo = 2341.42m;
+conta1.Saldo = conta2.Saldo * 0.7m;
+Console.WriteLine($"Número da conta: {conta1.Numero} com saldo: {conta1.Saldo}");
+Console.WriteLine($"Número da conta: {conta2.Numero} com saldo: {conta2.Saldo}");
