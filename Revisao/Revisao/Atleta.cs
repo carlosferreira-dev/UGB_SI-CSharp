594966558f9265fd826ffec13c131d6cf7e70d89
Carlos Eduardo Ferreira <mail@carlosferreira.dev>
2023-09-19 22:01:25 -0300

    .

---
+++ Revisao/Revisao/Atleta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao
+{
+    public class Atleta
+    {
+        private string nome, objetivo;
+        private DateTime dataNascimento;
+        private Atleta(string nome)
+        {
+            this.nome = nome;
+        }
+        private Medida Evolucao(DateTime datainicio, DateTime datafim)
+        {
+            return null;
+        }
+    }
+}
---
+++ Revisao/Revisao/Medida.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao
+{
+    public class Medida
+    {
+        private float peso, altura;
+        private int coxaDireita, coxaEsquerda, bicepsDireito, bicepsEsquerdo;
+        private DateTime dataMedida;
+    }
+}
